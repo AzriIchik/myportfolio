2885f2a6fcfde6d02f6e8d5a0e41392133be87b7
unknown <azriperisiben96@gmail.com>
2022-07-16 13:09:13 +0800

    adding section1 and section2  profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import profilePhoto from "./assets/img/profilephoto.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="section1__container">
+        <div className="section1__filter"></div>
+        <Navbar></Navbar>
+        <div className="section1_content px-6 d-flex">
+          <div className="bd-highlight">
+            <img src={profilePhoto} className="profile-photo"></img>
+          </div>
+          <div className="ps-0 ps-md-5 pt-5 bd-highlight flex-grow-1">
+            <h2>MOHAMMAD AZRI BIN PERISIBEN</h2>
+            <p className="text-opacity">Fullstack Web-tech Enthusiast</p>
+            <dl className="section1_biodata py-4">
+              <dt className="text-opacity">AGE:</dt> <dd>26</dd>
+              <dt className="text-opacity">PHONE:</dt> <dd>014-6511665</dd>
+              <dt className="text-opacity">EMAIL:</dt>{" "}
+              <dd>azriperisiben96@gmail.com</dd>
+              <dt className="text-opacity">ADDRESS:</dt>{" "}
+              <dd>
+                Pangsapuri Ria, Jalan Bukit Mewah 31, <br /> Taman Bukit Mewah,
+                43000 Kajang, Selangor
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+      <div className="section2__container px-6 py-5">
+        <h2 className="mt-5">Hi There_</h2>
+        <p>
+          I am Junior Web developer able to build a Web presence from the ground
+          up - from concept, navigation, layout and programming to UX and SEO.
+          Skilled at writing well-designed, testable and efficient code using
+          current best practices in Web development. Fast learner, hard worker
+          and team player who is proficient in an array of scripting languages
+          and multimedia Web tools.
+        </p>
+        {/*Later change this into components*/}
+        <button className="btn btn-secondary rounded-pill py-3 px-4"><FontAwesomeIcon icon={faDownload} /> DOWNLOAD CV</button>
+        
+      </div>
+      <p style={{ color: "red", paddingTop: "1rem" }}>HELLO</p>
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [navAnimStyle, setnavAnimStyle] = useState({});
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      let scrollBarPos = this.scrollY;
+
+      let navAnimStyleData = {
+        container: {
+          backgroundColor: "rgba(255, 255, 255, 1)",
+          color: "black",
+        },
+        ul: {
+          transform: "translateY(0%)",
+        },
+      };
+
+      if (scrollBarPos > 50) {
+        setnavAnimStyle(navAnimStyleData);
+      } else {
+        setnavAnimStyle({});
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="navbar__container px-6" style={navAnimStyle.container}>
+        <ul style={navAnimStyle.ul}>
+          <li>HELLO</li>
+          <li>RESUME</li>
+          <li>PORTFOLIO</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
